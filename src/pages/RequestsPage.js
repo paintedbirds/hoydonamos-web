99c45e40e5a3335eade17adc336f41cc3e4a8605
nicocadq <nmachado.camejo@gmail.com>
2021-09-29 10:11:24 -0300

    Add Petitions UI

---
+++ src/pages/RequestsPage.js
@@ -0,0 +1,10 @@
+import Requests from 'components/Requests';
+import MainLayout from 'layouts/MainLayout';
+
+const RequestsPage = () => (
+  <MainLayout>
+    <Requests />
+  </MainLayout>
+);
+
+export default RequestsPage;
