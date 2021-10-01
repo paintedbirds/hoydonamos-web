c6c95d22daf128568299373336e55cfa6eb35833
nicocadq <nmachado.camejo@gmail.com>
2021-10-01 09:04:27 -0300

    Add Create Petition UI

---
+++ src/pages/CreatePetitionPage.js
@@ -0,0 +1,10 @@
+import CreatePetition from 'components/CreatePetition';
+import MainLayout from 'layouts/MainLayout';
+
+const CreatePetitionPage = () => (
+  <MainLayout>
+    <CreatePetition />
+  </MainLayout>
+);
+
+export default CreatePetitionPage;
