102e47ab8c015974e4f97a3ba5a49a4c47b439f3
alejandroGonGon <56725406+alejandroGonGon@users.noreply.github.com>
2021-11-09 08:24:43 -0300

    Update src/components/EmptyState/EmptyState.js
    Co-authored-by: Nicolas Machado <nmachado.camejo@gmail.com>

--- src/components/EmptyState/EmptyState.js
+++ src/components/EmptyState/EmptyState.js
@@ -1,12 +1,10 @@
 import styles from './EmptyState.module.scss';
 
-const EmptyState = ({icon, text}) => {
-  return (
+const EmptyState = ({ icon, text }) => (
     <div className={styles.emptyState}>
         {icon}
         <p>{text}</p>
     </div>
-  );
-};
+);
 
 export default EmptyState;
