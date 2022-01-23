a26ed96886fb7509020bf78b7615f17d9758cfee
nicocadq <nmachado.camejo@gmail.com>
2022-01-23 00:34:13 -0300

    Add delete donation confirmation modal

--- src/features/donations/components/DonationCard/DonationCard.js
+++ src/features/donations/components/DonationCard/DonationCard.js
@@ -1,7 +1,8 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { ReactComponent as Heart } from 'assets/heart.svg';
-import { DeleteIcon } from 'features/common';
+import { ConfirmationModal, DeleteIcon } from 'features/common';
 import Status from './Status';
 
 import styles from './DonationCard.module.scss';
@@ -10,29 +11,66 @@ const DonationCard = ({
   title,
   description,
   image,
-  id,
   status,
   showOptions,
   onDelete,
   onClick,
-}) => (
-  <button className={styles.container} type="button" onClick={onClick}>
-    {showOptions && (
-      <div className={styles.options}>
-        {<Status status={status} />}
-        {<DeleteIcon onClick={onDelete} />}
+}) => {
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+  const onDeleteClick = () => {
+    setShowDeleteModal(true);
+  };
+
+  const onDeleteConfirm = () => {
+    onDelete();
+    setShowDeleteModal(false);
+  };
+
+  const onDeleteCancel = () => {
+    setShowDeleteModal(false);
+  };
+
+  return (
+    <>
+      {showDeleteModal && (
+        <ConfirmationModal
+          title="Eliminar Donación"
+          content="¿Estás seguro que quieres eliminar esta donación? Una vez eliminada no podrás recuperar esta donación."
+          confirmContent="Eliminar"
+          onCancel={onDeleteCancel}
+          onConfirm={onDeleteConfirm}
+        />
+      )}
+      <div
+        className={styles.container}
+        onClick={onClick}
+        aria-hidden="true"
+        role="button"
+      >
+        {showOptions && (
+          <div className={styles.options}>
+            {<Status status={status} />}
+            {<DeleteIcon onClick={onDeleteClick} />}
+          </div>
+        )}
+        <div className="w-full rounded-lg mb-4">
+          <img
+            alt="Donación"
+            className={styles.image}
+            src={image}
+            load="lazy"
+          />
+        </div>
+        <div className="flex justify-between justify-center">
+          <h4 className={styles.title}>{title}</h4>
+          {!showOptions && <Heart className={styles.heart} onClick={onClick} />}
+        </div>
+        <p className={styles.description}>{description}</p>
       </div>
-    )}
-    <div className="w-full rounded-lg mb-4">
-      <img alt="Donación" className={styles.image} src={image} load="lazy" />
-    </div>
-    <div className="flex justify-between justify-center">
-      <h4 className={styles.title}>{title}</h4>
-      {!showOptions && <Heart className={styles.heart} onClick={onClick} />}
-    </div>
-    <p className={styles.description}>{description}</p>
-  </button>
-);
+    </>
+  );
+};
 
 DonationCard.defaultProps = {
   onClick: () => {},
@@ -45,7 +83,6 @@ DonationCard.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
-  id: PropTypes.number.isRequired,
   onClick: PropTypes.func,
   onDelete: PropTypes.func,
   showOptions: PropTypes.bool,
