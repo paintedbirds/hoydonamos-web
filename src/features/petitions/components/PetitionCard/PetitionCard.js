f1dfa8a00169185c8d65b0ca3fcaa34f38e6c33a
nicocadq <nmachado.camejo@gmail.com>
2022-01-25 19:33:46 -0300

    Add delete petition confirmation modal

--- src/features/petitions/components/PetitionCard/PetitionCard.js
+++ src/features/petitions/components/PetitionCard/PetitionCard.js
@@ -1,39 +1,68 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { ReactComponent as Phone } from 'assets/phone.svg';
 import { ReactComponent as Email } from 'assets/email.svg';
-import { DeleteIcon } from 'features/common';
+import { ConfirmationModal, DeleteIcon } from 'features/common';
 import Status from './Status';
 
 import styles from './PetitionCard.module.scss';
 
-const PetitionCard = ({ petition, showOptions, onDelete }) => (
-  <div className={styles.container}>
-    {showOptions && (
-      <div className={styles.options}>
-        {<Status status={petition.state} />}
-        {<DeleteIcon onClick={onDelete} />}
-      </div>
-    )}
-    <img className={styles.image} alt="Usuario" src={petition.user.image} />
-    <h4 className={styles.title}>
-      {petition.user.name} - {petition.subject}
-    </h4>
-    <p className={styles.description}>{petition.description}</p>
-    <div className={styles['contact-info']}>
-      {petition.user?.phone && (
-        <div className={styles['contact-item']}>
-          <Phone />
-          <span>{petition.user.phone}</span>
-        </div>
+const PetitionCard = ({ petition, showOptions, onDelete }) => {
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+  const onDeleteClick = () => {
+    setShowDeleteModal(true);
+  };
+
+  const onDeleteConfirm = () => {
+    onDelete();
+    setShowDeleteModal(false);
+  };
+
+  const onDeleteCancel = () => {
+    setShowDeleteModal(false);
+  };
+
+  return (
+    <>
+      {showDeleteModal && (
+        <ConfirmationModal
+          title="Eliminar Petición"
+          content="¿Estás seguro que quieres eliminar esta petición? Una vez eliminada no podrás recuperar esta petición."
+          confirmContent="Eliminar"
+          onCancel={onDeleteCancel}
+          onConfirm={onDeleteConfirm}
+        />
       )}
-      <div className={styles['contact-item']}>
-        <Email />
-        <span>{petition.user.email}</span>
+      <div className={styles.container}>
+        {showOptions && (
+          <div className={styles.options}>
+            {<Status status={petition.state} />}
+            {<DeleteIcon onClick={onDeleteClick} />}
+          </div>
+        )}
+        <img className={styles.image} alt="Usuario" src={petition.user.image} />
+        <h4 className={styles.title}>
+          {petition.user.name} - {petition.subject}
+        </h4>
+        <p className={styles.description}>{petition.description}</p>
+        <div className={styles['contact-info']}>
+          {petition.user?.phone && (
+            <div className={styles['contact-item']}>
+              <Phone />
+              <span>{petition.user.phone}</span>
+            </div>
+          )}
+          <div className={styles['contact-item']}>
+            <Email />
+            <span>{petition.user.email}</span>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    </>
+  );
+};
 
 PetitionCard.defaultProps = {
   onDelete: () => {},
