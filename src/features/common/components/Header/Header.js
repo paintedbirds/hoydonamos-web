97de47178116ca4f5dd028314746952adfc0d670
nicocadq <nmachado.camejo@gmail.com>
2022-02-20 01:51:33 -0300

    laptop view done

--- src/features/common/components/Header/Header.js
+++ src/features/common/components/Header/Header.js
@@ -7,13 +7,17 @@ import Nav from './Nav';
 
 const Header = () => (
   <div className={styles['header']}>
-    <div className={styles['header_logo']}>
-      <Link to="/">
-        <Logo className="h-10 w-10" />
-        <h1 className={styles.title}>Che, ¿hoy donamos?</h1>
-      </Link>
+    <div className={styles['header_content']}>
+      <div className={styles['header_logo']}>
+        <Link to="/">
+          <Logo className="h-10 w-10" />
+          <h1 className={`${styles['title']} ${styles['title-main']}`}>
+            Che, ¿hoy donamos?
+          </h1>
+        </Link>
+      </div>
+      <Nav />
     </div>
-    <Nav />
   </div>
 );
 
--- src/features/common/components/Header/Nav.js
+++ src/features/common/components/Header/Nav.js
@@ -12,14 +12,16 @@ const Nav = () => {
         <SubNav title="Donaciones" type="donation" />
         <SubNav title="Solicitudes" type="request" />
       </div>
-      <SubNav title={user.name} type="user" />
-      <img
-        src={user.image}
-        width="50px"
-        height="50px"
-        alt="user"
-        style={{ borderRadius: '50%' }}
-      />{' '}
+      <div className={styles['nav-account']}>
+        <SubNav title={user.name} type="user" />
+        <img
+          src={user.image}
+          width="50px"
+          height="50px"
+          alt="user"
+          style={{ borderRadius: '50%' }}
+        />{' '}
+      </div>
     </div>
   );
 };
