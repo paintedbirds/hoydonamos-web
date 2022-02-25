275d046f614179f8d2f1b2a5018f33c97433444b
rubenderigo <rm.derigo@gmail.com>
2022-02-24 22:10:17 -0300

    fix to footer to bottom, add re-direct instagram

--- src/features/common/components/Footer/Footer.js
+++ src/features/common/components/Footer/Footer.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import { ReactComponent as Logo } from 'assets/logo--secondary.svg';
 import { ReactComponent as InstagramIcon } from 'assets/instagram_icon.svg';
 import { ReactComponent as GmailIcon } from 'assets/gmail_icon.svg';
@@ -6,6 +8,11 @@ import { ReactComponent as PaintedBirdsLogo } from 'assets/painted_birds_logo.sv
 import styles from './Footer.module.scss';
 
 const Footer = () => {
+  const PATH_INSTAGRAM =
+    'https://instagram.com/hoydonamos.uy?utm_medium=copy_link';
+  const SUBJECT_MAIL = 'Asunto';
+  const MAIL = 'hoydonamos@gmail.com';
+
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
@@ -17,12 +24,16 @@ const Footer = () => {
           <p>2022 Copyright &copy; Todos los derechos reservados.</p>
         </div>
         <div className={styles.find__in}>
-          <p>Encontranos en: </p>
-          <InstagramIcon />
-          <GmailIcon />
+          <p>Encontranos en </p>
+          <a href={PATH_INSTAGRAM} target="_blank" rel="noreferrer">
+            <InstagramIcon />
+          </a>
+          <Link to={{ pathname: 'www.gmail.com' }} target="_blank">
+            <GmailIcon />
+          </Link>
         </div>
         <div className={styles.powered}>
-          <p>Powered by: </p>
+          <p>Powered by </p>
           <PaintedBirdsLogo />
         </div>
       </div>
