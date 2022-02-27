b8f2c2c7b38b46b3c00aa726ac1af1ce3c07c81d
rubenderigo <rm.derigo@gmail.com>
2022-02-27 20:46:45 -0300

    add MainLayout stylesheet

--- src/features/common/layouts/Main/MainLayout.js
+++ src/features/common/layouts/Main/MainLayout.js
@@ -2,13 +2,13 @@ import PropTypes from 'prop-types';
 
 import { Footer, Header, ShapesBackground } from 'features/common';
 
+import styles from './MainLayout.module.scss';
+
 const MainLayout = ({ children }) => (
   <>
     <ShapesBackground />
     <Header />
-    <main className="my-8 max-w-screen-lg mx-auto w-full relative min-h-screen">
-      {children}
-    </main>
+    <main className={styles.main}>{children}</main>
     <Footer />
   </>
 );
