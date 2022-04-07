3fbf52db3970198eb232f75bca8d65cefc59b417
rubenderigo <rm.derigo@gmail.com>
2022-04-07 20:26:03 -0300

    apply hide disclamier

--- src/features/common/components/Home/Home.js
+++ src/features/common/components/Home/Home.js
@@ -1,4 +1,5 @@
 import { useDonationsAndPetitions } from 'features/common';
+import { useAuth } from 'features/auth';
 import UserInfo from './UserInfo';
 import Donations from './Donations';
 import Petitions from './Petitions';
@@ -6,10 +7,12 @@ import Petitions from './Petitions';
 const Home = () => {
   const { donationsStatus, donationsData, petitionsStatus, petitionsData } =
     useDonationsAndPetitions();
+  const { user } = useAuth();
+  const missingUserInfo = !user.phone || !user.about_me;
 
   return (
     <div className="px-6 mt-11">
-      <UserInfo />
+      {missingUserInfo && <UserInfo />}
       <Donations status={donationsStatus} data={donationsData} />
       <Petitions status={petitionsStatus} data={petitionsData} />
     </div>
